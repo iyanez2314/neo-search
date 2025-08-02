1b44ef6448bcee4366fc0a90478c0b63d39372b6
Isaac Yanez <isaac231467@gmail.com>
2025-08-02 18:08:24 -0500

    Add test_search function to debug search results and create TestSearch command

--- lua/neo-search/init.lua
+++ lua/neo-search/init.lua
@@ -23,5 +23,28 @@ function M.find_and_replace_debug()
 	telescope.find_and_replace_debug()
 end
 
+-- Test function to debug search results
+function M.test_search()
+	local config = require("neo-search.config")
+	local utils = require("neo-search.utils")
+	
+	local search_term = vim.fn.input("Test search for: ")
+	if search_term == "" then
+		print("No search term provided")
+		return
+	end
+	
+	local results = utils.search_in_buffer(search_term, config.options.search)
+	
+	print("\n=== SEARCH RESULTS ===")
+	print("Search term:", search_term)
+	print("Total results:", #results)
+	
+	for i, result in ipairs(results) do
+		print(string.format("%d. %s", i, result.display))
+	end
+	print("======================")
+end
+
 -- Export the module
 return M
